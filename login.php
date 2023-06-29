b63594ee40520a8409c5d6d1eb3556a12b433bb1
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-06-29 14:41:34 +0100

    feat: added login and logout functionalities

---
+++ login.php
@@ -0,0 +1,69 @@
+<?php
+
+require "includes/user_login.php";
+
+// initializes the session
+session_start();
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    // filters the username and password inputs gotten from the site
+    $inputed_username = filter_input(INPUT_POST, 'username');
+    $inputed_password = filter_input(INPUT_POST, 'password');
+
+    $login_data = fetchLoginData($conn, $inputed_username);
+
+    if ($login_data && password_verify($inputed_password, $login_data['password'])) {
+
+        // this helps prevent session fixation attacks
+        session_regenerate_id(true);
+
+        $_SESSION['is_logged_in'] = true;
+
+        // redirect to the admin page
+        header('Location: http://localhost/students_management-app/admin.php');
+        exit;
+    } else {
+
+        $error = "Login incorrect";
+    }
+}
+
+?>
+
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./stylings/login.css">
+</head>
+
+<body>
+
+    <a href="http://localhost/students_management-app/index.php"><h1>LexiSchool Management System</h1></a>
+    <br>
+    <h2>LOGIN</h2>
+
+    <?php if (!empty($error)) : ?>
+        <p align="center" style="color: white;"><?= $error ?></p>
+    <?php endif; ?>
+
+    <div class="container">
+        <form method="POST" action="">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username">
+            <br> <br>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+            <br> <br>
+
+            <center><button type="submit">Sign in</button></center>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ student_data.php
@@ -0,0 +1,76 @@
+<?php
+
+require "includes/db_connect.php";
+require "includes/get_student_data_id.php";
+
+// connect to the database server
+$conn = connectDB();
+
+// This gets the id from the browser tab when the save button was clicked in the new article page
+if (isset($_GET['id'])){
+
+    // Reading from the database to get specific article row by their id
+    $data = getStudentData($conn, $_GET['id']); // this holds an associative array
+    
+} else {
+    // no error message printed when there's no id included in the url link
+    $data = null; 
+}
+
+
+
+// download to pdf file
+if (isset($_POST['download'])){
+
+    header("Location: http://localhost/students_management-app/print_pdf.php?id={$data['id']}");
+    exit;
+}
+
+?>
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LEXISCHOOL</title>
+    <link href="./stylings/student_data.css" rel="stylesheet">
+</head>
+<body style="background-color: #371777; font-family: Arial, sans-serif;">
+
+    <h1 class="heading"><a href="http://localhost/students_management-app/index.php" style="text-decoration: none">LexiSchool Management System</a></h1>
+
+    <div>
+        <?php if ($data !== null): ?>
+
+            <article>
+                <center>
+                <?php if ($data["image_file"]): ?>
+                    <img src="http://localhost/students_management-app/uploads/<?= $data["image_file"]; ?>" alt="">
+                <?php endif; ?>
+                </center>
+
+                <h2><?= htmlspecialchars($data["student_name"]); ?></h2> 
+                <p><b>ID:</b> <?= htmlspecialchars($data["id"]); ?></p>
+                <p>Email: <?= htmlspecialchars($data["email"]); ?></p>
+                <p>Username: <?= htmlspecialchars($data["username"]);?></p>
+                <p>Grade: <?= $data["grade"];?></p>
+                <p>Class: <?= $data["class"];?></p>
+
+                <form action="" method="POST">
+                    <button id="pdf" type="submit" name="download">Download PDF</button>
+                </form>
+
+            </article>
+
+            <p class="booking_link"><i>Click here to return to the <a href="http://localhost/students_management-app/index.php">website</a></i></p>
+            
+        
+        <?php else: ?>
+            <p>No data found.</p>
+        <?php endif; ?>
+    </div>
+    
+    
+</body>
+</html>
